ae778f1636b43158645dc5e0500c53e0561b55bc
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2019-12-04 23:50:49 -0500

    Added Leevers

--- Enemies.py
+++ Enemies.py
@@ -1,14 +1,39 @@
+'''
+===============================================================================
+ENGR 133 Program Description 
+	Enemies class, which handles instancing of enemies and control of their 
+    actions.
+
+Assignment Information
+	Assignment:     Individual Project
+	Author:         Blake Lowe, lowe77@purdue.edu
+	Team ID:        002-10
+===============================================================================
+'''
+
+
+'''
+===============================================================================
+ACADEMIC INTEGRITY STATEMENT
+    I have not used source code obtained from any other unauthorized
+    source, either modified or unmodified. Neither have I provided
+    access to my code to another. The project I am submitting
+    is my own original work.
+===============================================================================
+'''
 import random
 import time
 
 class Enemy:
-    def __init__(self, num, name, hp, actionSpeed):
+    def __init__(self, num, name, hp, actionSpeed, actionDelay):
         self.name = name
-        self.num = num
+        self.num = str(num)
         self.hp = hp
         self.drops = [None]
         self.actionSpeed = actionSpeed#seconds per action
         self.nextActionTime = actionSpeed*random.random()
+        self.actionDelay = actionDelay
+        self.isAttackable = True
 
     def is_alive(self):
         return self.hp > 0
@@ -21,8 +46,8 @@ class Enemy:
 
 class Moblin(Enemy):
     def __init__(self, number):
-        super().__init__(number, "Moblin", 2, 4)
-        self.drops = [None, None, None, None, None, "heart", "heart", "heart", "heart", "fairy"]
+        super().__init__(number, "Moblin", 2, 4, 4)
+        self.drops = ["rupy", "rupy", "rupy", "rupy", "rupy", "heart", "heart", "heart", "heart", "fairy"]
 
     def action(self, player):
         self.nextActionTime += self.actionSpeed
@@ -42,8 +67,8 @@ class Moblin(Enemy):
 
 class Tektite(Enemy):
     def __init__(self, number):
-        super().__init__(number, "Tektite", 0.5, 4)
-        self.drops = [None, None, None, None, None, "heart", "heart", "heart", "heart", "fairy"]
+        super().__init__(number, "Tektite", 0.5, 4, 1)
+        self.drops = ["rupy", "rupy", "rupy", "rupy", "rupy", "heart", "heart", "heart", "heart", "fairy"]
         self.isJumpAttack = False
 
     def action(self, player):
@@ -54,12 +79,12 @@ class Tektite(Enemy):
                 return f"You dodged Tektite {self.num}."
             else:
                 player.hp -= 0.5
-                return f"Tektite {self.num} bites you. You lose 0.5 hearts\nYou have {player.hp} hearts remaining."
+                return f"Tektite {self.num} bites you! You lose 0.5 hearts\nYou have {player.hp} hearts remaining."
         i = random.randint(0, 100)
         if i < 33:
-            self.nextActionTime += 1.6
+            self.nextActionTime += 1.75
             self.isJumpAttack = True
-            return f"Tektite {self.num} jumps toward you. Dodge to avoid it's attack!"
+            return f"Tektite {self.num} jumps toward you. DODGE to avoid it's attack!"
         else:
             self.nextActionTime += self.actionSpeed
             if player.isEngaged(self):
@@ -67,11 +92,44 @@ class Tektite(Enemy):
                 player.target_num = ""
                 return f"Tektite {self.num} jumps away. You are no longer engaged with Tektite {self.num}"
             else:
-                return f"Tektite {self.num} jumps through the air."
+                return f"Tektite {self.num} jumps around."
 
 
-class Leever(Enemy):
+class Leever(Enemy):#TODO
     def __init__(self, number):
-        super().__init__(number, "Leever", 0.5, 4)
-        self.drops = [None, None, None, None, None, "heart", "heart", "heart", "heart", "fairy"]
-        
+        super().__init__(number, "Leever", 4, 4, 4)
+        self.drops = ["rupy", "rupy", "rupy", "rupy", "rupy", "5 rupies", "5 rupies", "clock", "heart", "heart"]
+        self.isAttackable = True
+
+    def burrow(self, player):
+        self.isAttackable = False
+        if player.isEngaged(self):
+            player.target_name = ""
+            player.target_num = ""
+            return f"Leever {self.num} burrows into the ground. You are no longer engaged with Leever {self.num}"
+        else:
+            return f"Leever {self.num} burrows into the ground."
+
+    def action(self, player):
+        self.nextActionTime += self.actionSpeed
+        if not self.isAttackable:
+            if random.random() < .5:
+                player.target_name = self.name
+                player.target_num = self.num
+                self.isAttackable = True
+                return f"Leever {self.num} surfaces in front of you. You are now engaged with Leever {self.num}."
+            else:
+                self.isAttackable = True
+                return f"Leever {self.num} surfaces."
+        else:
+            if player.isEngaged(self):
+                if random.random() < .5:
+                    if not player.isDodging:
+                        player.hp -= 1
+                        return f"Leever {self.num} attacks you! You lose 1 heart.\nYou have {player.hp} hearts remaining."
+                    else:
+                        return f"You dodged Leever {self.num}."
+                else:
+                    return self.burrow(player)
+            else:
+                return self.burrow(player)
